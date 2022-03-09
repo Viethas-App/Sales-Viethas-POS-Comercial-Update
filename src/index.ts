5de273b3b44562ee0a8bbaaf461c964b15ea63db
huyleduc81vn <43083377+huyleduc81vn@users.noreply.github.com>
2022-03-09 10:50:28 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -85,11 +85,62 @@ autoUpdater.on('update-downloaded', (info) => {
   win.webContents.send('download_progress', 'success')
 });
 
+// printer
+
+let print: BrowserWindow // khởi tạo cửa sổ để in, ở chế độ ẩn
+function createPrinttWindow() {
+  print = new BrowserWindow({
+    title: 'Print',
+    show: false, // chế độ ẩn
+    width: 800,
+    height: 600,
+    parent: myCapacitorApp.getMainWindow(),
+    webPreferences: {
+      nodeIntegration: true, //require
+    }
+  })
+  print.loadURL(`file://${__dirname}/print.html`)  // load file này để show dữ liệu html nhận từ angular gửi xuống để custom in
+
+  initPrintEvent()
+
+  print.on('close', () => {
+    print = null
+  })
+}
+function initPrintEvent() { // khởi tạo sự kiện in
+
+  //** nhận sự kiện từ angular gửi xuống, để lấy mấy in, lấy dánh sách máy in gửi ngược lại cho angular */
+  ipcMain.on('allPrint', () => {
+    console.log('received getPrinters msg');
+    const printers = print.webContents.getPrinters();
+    myCapacitorApp.getMainWindow().webContents.send('printName', printers)
+  })
+
+  //** nhận sự kiện in từ angular, rồi gửi qua trang print.html để chỉnh trang in */, bên trang print.html edit xong, gửi lại sự kiên "do" để thực hiện in
+  ipcMain.on('print-start', (event, obj) => {
+    console.log('print-start')
+    print.webContents.send('print-edit', obj);
+  })
+  //** nhận sự kiện "do" từ print.html để in */
+  ipcMain.on('do', (event, printer) => {
+    const options = {
+      silent: true,
+      deviceName: printer.deviceName,
+    }
+    print.webContents.print(options, (success, errorType) => {
+      if (!success) myCapacitorApp.getMainWindow().webContents.send('print-done', false); // gửi sự kiện lên angular nếu in lỗi
+      myCapacitorApp.getMainWindow().webContents.send('print-done', true); // gửi sự kiện lên angular nếu in ok
+    })
+  })
+}
+
+
 
 app.on('ready', function () {
   myCapacitorApp.init()
   // Create the Menu
   Menu.setApplicationMenu(null);
+  createPrinttWindow()
   autoUpdater.checkForUpdates();
 });
 app.on('window-all-closed', () => {
